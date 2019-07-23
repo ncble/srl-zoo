341671f13b4be8e602e1aa9504a6f692729bfc43
lulin <ncble17@gmail.com>
2019-07-23 15:31:21 +0200

    clean code

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 from .base_models import CustomCNN, UNet
 from .modules import SRLModules
-from .priors import SRLConvolutionalNetwork, SRLDenseNetwork, SRLCustomCNN, Discriminator
-from .supervised import ConvolutionalNetwork, DenseNetwork, CustomCNN
+from .supervised import ConvolutionalNetwork, DenseNetwork
 from .autoencoders import LinearAutoEncoder, DenseAutoEncoder, CNNAutoEncoder
 from .vae import DenseVAE, CNNVAE
-#from .gan import Generator, Discriminator, Encoder
+
